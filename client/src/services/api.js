f8abe8ba2d9358f13609c2d6e10f7ff271793a57
mac <mac@macs-MacBook-Air.local>
2020-11-04 17:54:51 +0200

    123

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 export default () => {
     return axios.create({
+        method: 'post',
         baseURL: 'https://rsve.herokuapp.com/',
         headers: {
             get: {        // can be common or any other method
