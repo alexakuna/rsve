3915c07dc6588fb31c255c6666c07a414a8283c3
mac <mac@macs-MacBook-Air.local>
2020-11-04 18:54:47 +0200

    123123123

--- routs/auth.js
+++ routs/auth.js
@@ -2,7 +2,7 @@ const express = require('express')
 const controller = require('../controlers/auth')
 const router = express.Router()
 
-router.post('/', controller.login)
+router.get('/', controller.login)
 //router.post('/register', controller.register)
 
 module.exports = router;
