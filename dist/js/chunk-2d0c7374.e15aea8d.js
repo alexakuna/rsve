2f344746fd79d3fce3a7a2e3109fae088172cdfa
mac <alexxjc@gmail.com>
2020-12-16 02:26:28 +0200

    works start push to heroku 2

---
+++ dist/js/chunk-2d0c7374.e15aea8d.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c7374"],{5058:function(e,t,n){"use strict";n.r(t);var c=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",[n("div",{staticClass:"page-title"},[n("h3",[e._v("Заявка")])])])}],s={name:"record",methods:{},mounted:function(){}},a=s,i=n("2877"),u=Object(i["a"])(a,c,r,!1,null,"27929e82",null);t["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d0c7374.e15aea8d.js.map
